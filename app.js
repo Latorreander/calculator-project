16c37fe98f82edfc241571fb2a451f5ad4321d4f
Latorreander <latorreander@gmail.com>
2023-05-20 22:21:32 -0300

    'adicionando botões'

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const inputArea = document.querySelector('.display')
 const divButtons = document.querySelector('.buttons')
+const powerButton = document.querySelector('.power')
+const sound = document.getElementById('sound')
+const soundButton = document.querySelector('.btn-sound')
 
 
 divButtons.addEventListener('click', e => {
 
-  
 
     if(e.target.value === '='){
       
@@ -14,9 +16,41 @@ divButtons.addEventListener('click', e => {
     }
   
     inputArea.value += e.target.value
+   
     
     if (e.target.value === 'C'){
-        inputArea.value = 0
-        // window.location.reload()
-    }  
+       location.reload()
+       
+    }
+
+    if (e.target.classList.contains('power')){
+       inputArea.classList.toggle('off-mode')
+       divButtons.classList.toggle('off-mode')
+    }
+
+   
+})
+
+
+powerButton.addEventListener('click', () => {
+
+    if(powerButton){
+       inputArea.value = ''
+        inputArea.classList.toggle('off-mode')
+        divButtons.classList.toggle('off-mode')
+    }
+
+})
+
+soundButton.addEventListener('click', () =>{
+    let soundEffect = true
+
+    if(soundEffect){
+        sound.play()
+        soundEffect = false
+
+    }else{
+        sound.pause()
+         soundEffect = true
+    }
 })
